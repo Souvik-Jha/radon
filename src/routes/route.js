6f13658ccea43a0b4d7530d5c91a6aed2490c385
Souvik Jha <jhasouvik4@gmail.com>
2022-06-02 23:03:03 +0530

    delete of class task

--- src/routes/route.js
+++ src/routes/route.js
@@ -43,27 +43,6 @@ router.get('/hello', function (req, res) {
 })
 
 
-// router.get("/sol1", function (req, res) {
-//     console.log("assaassa")
-//     //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
-//     let arr= [1,2,3,5,6,7]
-//     let sum = 0
-//     for(i = 0; i<arr.length ; i++){
-//          sum += arr[i]
-//     }
-//     console.log(sum)
-
-//     let n = arr[arr.length-1]
-//     console.log(n)
-//     let totalSum = (n.n+1)/2
-//     console.log(totalSum)
-//     // let missingNumber = n.n+1/2 - sum
-//     // console.log(missingNumber)
-
-
-//     ///LOGIC WILL GO HERE no
-//     res.send("missingNumber");
-// });
 
 
 module.exports = router;
