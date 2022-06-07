85a0e7c6cb3e7cdeaade9b12c7a378b74bc70c0d
Souvik Jha <jhasouvik4@gmail.com>
2022-06-07 11:42:46 +0530

    completed assignment

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -3,7 +3,6 @@ const BookModel= require("../models/bookModel")
 
 const createBook= async function (req, res) {
     let data= req.body
-
     let savedData= await BookModel.create(data)
     res.send({msg: savedData})
 }
@@ -15,16 +14,21 @@ const bookList= async function (req, res) {
 
 
 const getBooksInYear = async function(req, res) {
-    let year = req.body
-    let booksInYear = await BookModel.find(year)
+    let year = req.body.year
+    let booksInYear = await BookModel.find({year: year})
     res.send({msg: booksInYear})
 }
 
-// const getXINRBooks = async function (req, res) {
-//     let x = BookModel.prices
-//     let booksINR = await BookModel.find({x : "500INR"})
-//     res.send({ msg: booksINR })
-// }
+const getParticularBooks = async function(req, res) {
+    let condition = req.body
+    let booksInYear = await BookModel.find(condition)
+    res.send({msg: booksInYear})
+}
+
+const getXINRBooks = async function (req, res) {
+    let booksINR = await BookModel.find({$or: [{"prices.indianPrice": "500INR"}, {"prices.indianPrice": "200INR"}, {"prices.indianPrice": "100INR"}]})
+    res.send({ msg: booksINR })
+}
 
 const getRandomBooks= async function (req, res) {
     let randomBooks= await BookModel.find({$or: [{stockAvalable: true}, {totalPages:{$gt: 500}}]})
@@ -105,5 +109,6 @@ const getRandomBooks= async function (req, res) {
 module.exports.createBook= createBook
 module.exports.bookList= bookList
 module.exports.getBooksInYear= getBooksInYear
-// module.exports.getXINRBooks= getXINRBooks
+module.exports.getParticularBooks= getParticularBooks
+module.exports.getXINRBooks= getXINRBooks
 module.exports.getRandomBooks= getRandomBooks
