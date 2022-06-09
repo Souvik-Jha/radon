e4901c7ef16149a4042e8c68a114e7135fffd85b
Souvik Jha <jhasouvik4@gmail.com>
2022-06-09 23:04:40 +0530

    assignment done

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,23 +1,45 @@
 const authorModel = require("../models/authorModel")
+const { required } = require("nodemon/lib/config")
 const bookModel= require("../models/bookModel")
+const publisherModel= require("../models/publisherModel")
+
 
 const createBook= async function (req, res) {
     let book = req.body
+    let authorId=await authorModel.findById(book.author)
+    let publisherId = await publisherModel.findById(book.publisher)
+    if(!book.author){
+    res.send("author details are required")
+    } else if( !authorId){
+    res.send("author is not present")
+    }else if(!book.publisher) {
+    res.send("publisher details are required")
+    }else if(!publisherId){
+    res.send("publisher is not present")
+    }else{
     let bookCreated = await bookModel.create(book)
     res.send({data: bookCreated})
+    }
 }
 
-const getBooksData= async function (req, res) {
-    let books = await bookModel.find()
-    res.send({data: books})
-}
 
-const getBooksWithAuthorDetails = async function (req, res) {
-    let specificBook = await bookModel.find().populate('author_id')
+const getBooks = async function (req, res) {
+    let specificBook = await bookModel.find().populate(['author','publisher'] )
     res.send({data: specificBook})
-
 }
+
+const hardCoverTrue =async function(req,res) {
+    let firstUpdate= await bookModel.updateMany({},{isHardCover:false})
+    let updateCover= await bookModel.updateMany(
+        {$or:[{publisher:"62a1c145f8dd1f232c579049"},{publisher:"62a1de9e337da8bd7a935862"}]},
+        {$set:{isHardCover:true}},
+        )
+    let UpdatedBook = await bookModel.updateMany(
+    )
+    res.send({data: updatedBook})
+}
+
 
 module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
-module.exports.getBooksWithAuthorDetails = getBooksWithAuthorDetails
+module.exports.getBooks = getBooks
+module.exports.hardCoverTrue = hardCoverTrue
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Souvik_Jha:Govtjob1@cluster0.zb2phjl.mongodb.net/book-author-publisher?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
