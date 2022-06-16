715b634d05432cc55997223693f0a0118eb021ed
Souvik Jha <jhasouvik4@gmail.com>
2022-06-16 22:12:55 +0530

    assignment done

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,20 +1,26 @@
 const jwt = require("jsonwebtoken");
 
 let userValidation = function(req, res, next){
-    let token = req.headers["x-Auth-token"];
+   try{ let token = req.headers["x-Auth-token"];
     if (!token) token = req.headers["x-auth-token"];
-    if (!token) return res.send({ status: false, msg: "token must be present" });
+    if (!token) return res.status(401).send({ status: false, msg: "token must be present" });
     // console.log(token);
     
 
     let decodedToken = jwt.verify(token, "functionup-radon");
     if (!decodedToken)
-      return res.send({ status: false, msg: "token is invalid" });
+      return res.status(401).send({ status: false, msg: "token is invalid" });
       next()
+}
+catch (err) {
+    console.log("This is the error :", err.message)
+    res.status(500).send({ msg: "Error", error: err.message })
+}
 }
 
 
 let authorization = function(req,res,next){
+    try{
     let token = req.headers["x-Auth-token"];
     if (!token) token = req.headers["x-auth-token"];
     let decodedToken = jwt.verify(token, 'functionup-radon')
@@ -24,8 +30,12 @@ let authorization = function(req,res,next){
         let userLoggedIn = decodedToken.userId
     
         //userId comparision to check if the logged-in user is requesting for their own data
-        if(userToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
+        if(userToBeModified != userLoggedIn) return res.status(403).send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
         next()
+    }catch (err) {
+        console.log("This is the error :", err.message)
+        res.status(500).send({ msg: "Error", error: err.message })
+    }
 }
 
 
